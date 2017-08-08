6c381168961128a5054520b7fbcbe14b388b12ea
Alexey Grachov <grachov.alexey@gmail.com>
2017-08-08 21:04:45 +0300

    Add emojies as constants.

--- tools.go
+++ tools.go
@@ -1,5 +1,18 @@
 package bstools
 
+// Buildings emojies
+const (
+	TownhallEmoji  = "\u1F3E4" // 🏤
+	StorageEmoji   = "\u1F3DA" // 🏚
+	HousesEmoji    = "\u1F3D8" // 🏘
+	FarmEmoji      = "\u1F33B" // 🌻
+	SawmillEmoji   = "\u1F332" // 🌲
+	MineEmoji      = "\u26CF"  // ⛏
+	BarracksEmoji  = "\u1F6E1" // 🛡
+	TrebuchetEmoji = "\u2694"  // ⚔
+)
+
+// Buildings
 const (
 	Townhall = "🏤 Town hall"
 	Storage  = "🏚 Storage"
